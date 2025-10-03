653b73bdf3c08ff89e56a56a554f7867a239f1c9
Scanf-s <calzone0404@gmail.com>
2025-10-03 22:24:34 +0900

    feat: add Dockerfile to deploy

--- cmd/ssu-announcement-notifier/main.go
+++ cmd/ssu-announcement-notifier/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
 
 	"ssu-announcement-notifier/config"
 	"ssu-announcement-notifier/internal/scraper"
@@ -9,14 +14,40 @@ import (
 )
 
 func main() {
-	log.Println("ssu-announcement-notifier")
+	// AWS_LAMBDA_FUNCTION_NAME 환경변수로 Lambda 환경 판별
+	if os.Getenv("AWS_LAMBDA_FUNCTION_NAME") != "" {
+		// Lambda 환경
+		lambda.Start(handleRequest)
+	} else {
+		// 로컬 환경
+		runLocal()
+	}
+}
+
+func handleRequest(ctx context.Context, event json.RawMessage) (string, error) {
+	log.Println("Running in Lambda environment")
 
 	// 환경변수 불러오기
 	cfg := config.LoadConfig()
 
 	// 숭실대학교 공지사항 스크래핑
-	var resultHtml []byte
-	resultHtml = scraper.ScrapeSSUAnnouncements(cfg)
+	resultHtml := scraper.ScrapeSSUAnnouncements(cfg)
+
+	// 공지사항 HTML 파싱해서 원하는 정보 추출
+	parsedResult := ssu_announcement_parser.ParseSSUAnnouncementsHtml(resultHtml)
+	log.Println(parsedResult)
+
+	return "Success", nil
+}
+
+func runLocal() {
+	log.Println("Running in local environment")
+
+	// 환경변수 불러오기
+	cfg := config.LoadConfig()
+
+	// 숭실대학교 공지사항 스크래핑
+	resultHtml := scraper.ScrapeSSUAnnouncements(cfg)
 
 	// 공지사항 HTML 파싱해서 원하는 정보 추출
 	parsedResult := ssu_announcement_parser.ParseSSUAnnouncementsHtml(resultHtml)
