b079fd7caca722f3c22a4e7844f92c7a1ec30860
Ui Jong Yang <calzone0404@gmail.com>
2025-10-12 22:14:28 +0900

    feat: update eventworker (#7)
    * feat: add basic api development structure
    
    * feat: modify scraping period to every 3 hours
    
    * feat: add basic ssu-path dynamic parser
    
    * feat: add ssu_path scraper and applied goroutine with errgroun for parallel task
    
    * fix: modify chromium installation instructions
    
    * fix: modify chromium installation instructions (https://github.com/amazonlinux/amazon-linux-2023/discussions/417)
    
    * fix: remove quite option
    
    * fix: remove all quite options
    
    * fix: try another method
    
    * fix: install chrome directly
    
    * fix: modified chromium installation instruction
    
    * fix: apply deployment pipeline
    
    * fix: fetch envs from ssm parameter store and modify some infra configurations
    
    * fix: add error handling codes in when saving data
    
    * fix: add err handle logic when opening chromium browser
    
    * chore: add some logs for debugging
    
    * remove MustMethods and change to another
    
    * fix: add arguments on Click method
    
    * fix: updated chromium binary because of conflicts
    
    * fix: modify chromium path
    
    * fix: update chromium installation
    
    * fix: modify publish logic
    
    * fix: Change deploy process to use zip binary file
    
    * fix: Modify environment variable setup
    
    * fix: Change cicd pipeline and Makefile
    
    * fix: delete docker installation process
    
    * feat: Add go setup process

---
+++ Makefile
@@ -0,0 +1,26 @@
+EVENTWORKER_LAMBDA_FUNCTION_NAME ?= asdf
+AWS_REGION ?= ap-northeast-2
+
+# Eventworker 빌드
+# -s -w 옵션 추가해서 디버깅 정보 제거하면 바이너리 크기가 줄어든다
+build-eventworker:
+	@echo "Building Eventworker service"
+	cd eventworker && GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o bootstrap ./cmd/eventworker/main.go
+	cd eventworker && zip eventworker.zip bootstrap
+	@echo "Eventworker build completed"
+
+# Eventworker 배포
+deploy-eventworker:
+	@echo "Deploying Eventworker service"
+	aws lambda update-function-code \
+	--function-name $(EVENTWORKER_LAMBDA_FUNCTION_NAME) \
+	--zip-file fileb://eventworker/eventworker.zip \
+	--region $(AWS_REGION)
+	@echo "Eventworker deployment completed"
+
+# Eventworker 정리
+clean-eventworker:
+	@echo "Cleaning up"
+	rm -f eventworker/bootstrap
+	rm -f eventworker/eventworker.zip
+	@echo "Cleanup completed"
--- eventworker/internal/service/send_message.go
+++ eventworker/internal/service/send_message.go
@@ -12,37 +12,67 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
-func SendMessageToSqs(ctx context.Context, cfg *config.AppConfig, message dto.Message) error {
+func SendMessageToSqs[T dto.Message](ctx context.Context, cfg *config.AppConfig, message T) error {
+
 	// Message를 JSON으로 직렬화
 	messageBody, err := json.Marshal(message)
 	if err != nil {
+		log.Printf("Failed to serialize message: %v\n", err)
 		return err
 	}
 
 	// SQS로 새로 추가된 데이터 전송 (notifier가 이거 받아서 알림 발송)
-	// Notifier에서 json.Unmarshal(message.Body, &announcement)로 사용해주면 됨
-	// 메세지 구조
-	// {
-	//    "Body": "{\"Link\":\"https://...\",\"Category\":\"학사\", ...}",
-	//    "MessageAttributes": {
-	//      "Category": "학사" -> 이걸로 필터링해서 구독자한테 알람 전송
-	//    }
-	// }
-	input := &sqs.SendMessageInput{
-		QueueUrl:    &cfg.QueueUrl,
-		MessageBody: aws.String(string(messageBody)),
-		MessageAttributes: map[string]types.MessageAttributeValue{
+	// Notifier에서 json.Unmarshal(message.Body, &announcement or &ssu_path)로 사용해주면 됨
+	var messageAttributes map[string]types.MessageAttributeValue
+	switch messageType := message.GetMessageType(); messageType {
+	case dto.MessageTypeAnnouncement:
+		// AnnouncementMessage로 타입 설정
+		announcementMsg, ok := any(message).(dto.AnnouncementMessage)
+		if !ok {
+			log.Println("failed to cast message to AnnouncementMessage")
+			return nil
+		}
+		// AnnouncementMessage의 경우 Category 속성으로 구독자별로 이메일 전송하므로 필요
+		messageAttributes = map[string]types.MessageAttributeValue{
+			"MessageType": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(string(dto.MessageTypeAnnouncement)),
+			},
 			"Category": {
 				DataType:    aws.String("String"),
-				StringValue: aws.String(message.Category),
+				StringValue: aws.String(announcementMsg.Category),
 			},
-		},
+		}
+	case dto.MessageTypeSSUPath:
+		// SSUPathMessage로 타입 설정
+		_, ok := any(message).(dto.SSUPathMessage)
+		if !ok {
+			log.Println("failed to cast message to SSUPathMessage")
+			return nil
+		}
+		messageAttributes = map[string]types.MessageAttributeValue{
+			"MessageType": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(string(dto.MessageTypeSSUPath)),
+			},
+		}
+	default:
+		log.Printf("unsupported message type: %s", messageType)
+		return nil
+	}
+
+	input := &sqs.SendMessageInput{
+		QueueUrl:          &cfg.QueueUrl,
+		MessageBody:       aws.String(string(messageBody)),
+		MessageAttributes: messageAttributes,
 	}
 
 	result, err := cfg.SqsClient.SendMessage(ctx, input)
 	if err != nil {
+		log.Printf("Failed to send message to SQS: %v\n", err)
 		return err
 	}
-	log.Printf("Message sent to SQS: %s\n", *result.MessageId)
+
+	log.Printf("Message sent to SQS: %s", *result.MessageId)
 	return nil
 }
